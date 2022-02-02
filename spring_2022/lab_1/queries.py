e45be0586bc88e88fccba7b3e2b844a1ec221f93
Amadou Ngom <amlatyrngom@protonmail.com>
2022-02-02 09:44:51 -0500

    Fix query file

--- spring_2022/lab_1/queries.py
+++ spring_2022/lab_1/queries.py
@@ -2,11 +2,13 @@ import sqlite3 as sql
 import pandas as pd
 import argparse
 
-def runSQL(query_num):
-	with sql.connect("lab1.sqlite") as conn, open("queries/q{}.sql".format(query_num)) as in_query:
-		cur = conn.cursor()
-		df = pd.read_sql_query(in_query.read(), conn)
-		return df
+def runSQL(query_num, store):
+    with sql.connect("data/imdb.db") as conn, open("queries/q{}.sql".format(query_num)) as in_query:
+        cur = conn.cursor()
+        df = pd.read_sql_query(in_query.read(), conn)
+        pd.set_option('display.max_rows', 100)
+        if store: df.to_csv(f"submission/sql_sol{query_num}.csv", index=False)
+        return df
 
 def Q1Pandas():
     """
@@ -45,10 +47,14 @@ def Q6Pandas():
     return None
 
 pandas_queries = [Q1Pandas, Q2Pandas, Q3Pandas, Q4Pandas, Q5Pandas, Q6Pandas]
+df = None
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--query", "-q", help="Run a specific query", type=int)
+    parser.add_argument("--store", "-s", help="Store", default=False, action='store_true')
     args = parser.parse_args()
+
+    store = args.store
 
     queries = range(1, 11)
     if args.query != None:
@@ -57,7 +63,9 @@ if __name__ == "__main__":
         print("\nQuery {}".format(query))
         if query <= 6:
             print("\nPandas Output")
-            print(pandas_queries[query-1]())
+            df = pandas_queries[query-1]()
+            print(df)
+            if store: df.to_csv(f"submission/pandas_sol{query}.csv", index=False)
         print("\nSQLite Output")
-        df = runSQL(query)
+        df = runSQL(query, store)
         print(df)
