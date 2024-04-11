ca08d80584f3f86a77ecb4204cc7d025f01733ba
mdr223 <mdr223@cornell.edu>
2024-04-11 16:07:40 -0400

    dune partial push

--- lab_6/README.md
+++ lab_6/README.md
@@ -1,25 +1,46 @@
 Table of Contents
 =================
-- [Lab 6: Ray Dune](#lab-0-setting-up-your-environment)
+- [Lab 6: Ray Dune (The Battle for Arrakis)](#lab-6-ray-dune-the-battle-for-arrakis)
   * [1. Accessing Your EC2 Instance](#1-accessing-your-ec2-instance)
   * [2. Additional Setup](#2-additional-setup)
   * [3. Setup Lab 6 Environment](#3-setup-lab-6-environment)
-  * [4. Lab Overview](#4-lab-overview)
-    * [The Dataset](#the-dataset)
-    * [TODO: More Subsections (If Necessary)](#todo-more-subsections-if-necessary)
-  * [5. Submission Instructions](#6-submission-instructions)
+  * [4. The Battle for Arrakis](#4-the-battle-for-arrakis)
+    * [Spark (30 pts)](#spark-30-pts)
+    * [Ray (70 pts)](#ray-70-pts)
+  * [5. Submission Instructions](#5-submission-instructions)
     * [Before You Submit: Push Your Changes](#before-you-submit-push-your-changes)
     * [Submitting as an Individual](#submitting-as-an-individual)
     * [Submitting as a Group](#submitting-as-a-group)
 
 ---
-# Lab 6: Title Goes Here
+# Lab 6: Ray Dune (The Battle for Arrakis)
 ---
 * **Assigned: April 11th**
 * **Due: April 23rd, 11:59:00 PM ET.**
 
 
-image goes here
+![dune](dune.jpeg)
+
+---
+---
+---
+> *\"The person who controls ~~the spice~~ Ray controls the universe. A process cannot be understood by stopping it. Understanding must move with the flow of the process, must join it and flow with it.
+> \-- Dune\" \-- Your TAs Trying to Teach you Ray (and Spark)*
+---
+---
+---
+You awaken on the planet [Arrakis](https://dune.fandom.com/wiki/Arrakis) -- the home of the [Fremen](https://dune.fandom.com/wiki/Fremen) people -- amidst a terrible war started between members of [The Great Houses](https://dune.fandom.com/wiki/House). As depicted in Frank Herbert's [novels](https://en.wikipedia.org/wiki/Dune_novel), and the recent [Hollywood smash hit](https://en.wikipedia.org/wiki/Dune_franchise) trilogy, [Spice](https://dune.fandom.com/wiki/Spice_Melange) is the most valuable substance in the universe. Without Spice interplanetary travel is impossible. The Great Houses fight for control of your home planet Arrakis and its vital Spice fields.
+
+You are a fierce Fremen warrior and lead four groups of Fedaykin -- the most skilled of the Fremen fighters. In consultation with the [Reverend Mother](https://dune.fandom.com/wiki/Reverend_Mother) and leaders of the [Bene Gesserit](https://dune.fandom.com/wiki/Bene_Gesserit) (wise people), you come up with a clever plan to expel The Great Houses from Arrakis.
+
+The plan is simple: **destroy the Spice fields.**
+
+The destruction of Spice will bring an end to The Great Houses' war over Arrakis and allow peace to prevail on your home planet. However, in an effort to maximize the chances of the plan's success, the Bene Gesserit have gone behind your back and secretly funded another Fremen warrior -- **your rival**.
+
+You and your rival are given instructions to destroy the planet's Spice fields in opposite hemispheres (you in the northern hemisphere, your rival in the southern hemisphere). That being said, it is clear that the Bene Gesserit only intend for one of you to become the leader once the destruction is over. **In order to prevail as the leader of the Fremen, you must destroy more Spice fields than your rival.** Failure to do so will likely result in permanent exile from Arrakis.
+
+---
+Credit for [the inspiration of this lab](https://www.cs.cornell.edu/courses/cs3410/2016sp/projects/pa3/pa3.html) belongs to the CS 3410 course staff at Cornell
 
 ## 1. Accessing Your EC2 Instance
 If the number in your username modulo 2 equals:
@@ -75,11 +96,17 @@ $ bash setup.sh
 
 [*Back to top*](#table-of-contents)
 
-## 4. Lab Overview
+## 4. The Battle for Arrakis
 
-**Task 1** Filling the blanks in `spark-code/Task1.py` which is Spark program that output the top-100 most frequent words in Dune novel series by Frank Herbert using with Spark's RDD (https://spark.apache.org/docs/latest/rdd-programming-guide.html#rdd-operations) APIs.
-Your program should replace the set of punctuations (in bracket) with whitespace before start counting: [:=,!'".?] .
-Then your program should turn all the words into lower case.
+### Spark (30 pts)
+Before you and your Fremen warriors can leave to destroy the Spice fields of Arrakis, you decide it would be a good idea to read up on the planet's history. Unfortunately, your warriors are not great readers and are in a rush to leave, so you decide to create a condensed summary of the history for them.
+
+**Task 1** Fill in the blanks in `spark-code/Task1.py` which is Spark program that outputs the top-100 most frequent words in Dune novel series by Frank Herbert using [Spark's RDD APIs](https://spark.apache.org/docs/latest/rdd-programming-guide.html#rdd-operations).
+
+Your program should:
+1. first, replace the following set of punctuation [:=,!'".?] with whitespace
+2. then, lower case all of the words
+3. finally, compute the word frequencies
 
 Output the top-100 words along with their frequency in the following format to standard output:
     
@@ -92,12 +119,17 @@ Output the top-100 words along with their frequency in the following format to s
     alia, 1033
     now, 997
     ...
-    
 
-Note that if two words have the same frequency, break tie by outputting the lexigraphically smaller word first.
+Note that if two words have the same frequency, break the tie by outputting the lexigraphically smaller word first.
 
-**Task 2**  Building on top of Task 1, Filling the blanks in `spark-code/Task2.py` that output the top-100 most frenquent words in the novel that are not stop-words using Spark's RDD and DataFrame APIs.Your program should replace the set of punctuations (in bracket) with whitespace before start counting: [:=,!'".?]. Then your program should turn all the words into lower case.
-    
+**Task 2**  Building on top of Task 1, fill in the blanks in `spark-code/Task2.py` to output the top-100 most frequent words in the novel that are NOT stop-words using Spark's RDD and DataFrame APIs.
+
+Your program should:
+1. first, replace the set of punctuation [:=,!'".?] with whitespace
+2. then, lower case all of the words
+3. then, remove stop-words
+4. finally, compute the word frequencies
+
 Output the top-100 words along with their frequency in the following format to standard output:
     
     word,freq
@@ -113,7 +145,7 @@ Output the top-100 words along with their frequency in the following format to s
     asked, 817
     ...
     
-Note that if two words have the same frequency, break  tie by outputting the lexigraphically smaller word first.
+Note that if two words have the same frequency, break the tie by outputting the lexigraphically smaller word first.
 
 **How to submit Spark programs to our Spark Cluster**
 
@@ -128,8 +160,11 @@ Submission of Task2 can be done similarly.
 **Note about submission**
 When it's time to submit, you will need to zip the `spark-code` folder which includes the stdout/stderr log files of the programs along with the code. See the [section 5](#5-submission-instructions) for more detailed instructions.
 
-## 5. Submission Instructions
+### Ray (70 pts)
+TODO
 
+
+## 5. Submission Instructions
 ### Before You Submit: Push Your Changes
 Please make sure to push your code to your private repository:
 ```bash
@@ -137,14 +172,9 @@ $ git add -A # or, limit to the set of files you want to keep
 $ git commit -m "pushing final state of lab 6"
 $ git push origin main
 ```
-We may not use the same machine(s) from lab-to-lab so in order to keep your changes you will need to check them into your private repository. Labs are designed to be independent -- so if you forget to push your changes it should not be the end of the world -- but it's better to be safe than sorry.
-
-**What to submit for grading**
-
-Zip the `spark-code` folder which includes the stdout/stderr log files of the programs along with the code. 
 
 ### Submitting as an Individual
-To submit responses as an individual, simply run:
+To submit responses as an individual, you will need to zip your spark code and log files as well as your Dune game code. To do this, simply run:
 ```sh
 # Zip the contents of the submission folder
 zip -r submission.zip spark-code dune
@@ -153,7 +183,7 @@ zip -r submission.zip spark-code dune
 Submit the generated `submission.zip` file to Gradescope.
 
 ### Submitting as a Group
-To submit responses as a group, simply run:
+To submit responses as a group, you will need to zip your spark code and log files as well as your Dune game code. To do this, simply run:
 ```sh
 # Zip the contents of the submission folder;
 zip -r submission.zip spark-code dune
---
+++ lab_6/dune/dune_game.py
@@ -0,0 +1,307 @@
+import argparse
+import ray
+import time
+
+import numpy as np
+
+from dune_helpers import *
+from dune_rivals import *
+
+# TODO: double check that obj. store read is < 0.1 (S3 read time)
+# DEFINITIONS
+SLEEP_SECONDS_PER_TRAVEL_COORD = 0.001
+SLEEP_SECONDS_READ_FROM_S3     = 0.100
+SLEEP_SECONDS_NOT_ON_SPICE     = 1.000
+
+
+class BaseFedaykin:
+    """
+    DO NOT MODIFY
+
+    Base class with common functionality shared across all Fedaykin Actors.
+    """
+    def __init__(self, payload: str):
+        self.payload = payload
+        self.i = np.random.randint(0, MAP_DIM - 1)
+        self.j = np.random.randint(0, MAP_DIM - 1)
+        self.gamestate = ray.get_actor("GameState")
+        self.spice_loc_map = None
+        self.spice_file_map = None
+        self.order_map = None
+
+    def _destroy_spice_field(self) -> bool:
+        """
+        DO NOT MODIFY
+
+        (Contributes to) destroy(ing) the spice field at location: (self.i, self.j)
+
+        Recall that order_map[(i, j)] returns the order in which your Fedaykin must
+        call _destroy_spice_field() in order for the field to be fully destroyed.
+        (There is no partial credit for partial destruction).
+
+        The function will return False if the actor fails to destroy the spice field
+        because either:
+
+          A. (self.i, self.j) is not a valid spice field location, or
+          B. at least one Fedaykin preceding this one in the order_map has not yet
+             called _destroy_spice_field() at this location
+
+        The function returns True if the call to destroy the spice field is successful.
+        """
+        # if this isn't a spice field, incur a delay and return False
+        if not self.spice_loc_map[(self.i, self.j)]:
+            print(f"Fedaykin{self.id} tried to destroy spice at {(self.i, self.j)}, but this is not a Spice location.")
+            print(f"Fedaykin{self.id} sleeping for {SLEEP_SECONDS_NOT_ON_SPICE} seconds")
+            time.sleep(SLEEP_SECONDS_NOT_ON_SPICE)
+            return False
+
+        # if file is "on S3" simulate extra delay for the network request
+        if self.spice_file_map[(self.i, self.j)] == S3_FILE:
+            print(f"Fedaykin{self.id} fetching spice field object from S3 for {(self.i, self.j)}")
+            time.sleep(SLEEP_SECONDS_READ_FROM_S3)
+        else:
+            print(f"Fedaykin{self.id} fetching spice field object from OBJECT STORE for {(self.i, self.j)}")
+
+        # get spice field object
+        spice_field_refs = ray.get(self.gamestate.get_spice_field_refs.remote("northern"))
+        spice_field = ray.get(spice_field_refs[(self.i, self.j)])
+
+        # check if spice field object can be written to
+        write_order = self.order_map[(self.i, self.j)]
+        try:
+            write_idx = np.where(write_order == self.id)[0][0]
+        except:
+            print(f"Fedaykin{self.id} tried to destroy spice at {(self.i, self.j)} but is not a valid destroyer ({list(write_order)})")
+
+        if np.array_equal(spice_field["writes"], write_order[:write_idx]):
+            spice_field["writes"].append(self.id)
+            if np.array_equal(spice_field["writes"], write_order):
+                spice_field["payload"] = self.payload
+                print(f"Fedaykin{self.id} DESTROYED SPICE FIELD AT ({(self.i, self.j)})")
+            else:
+                print(f"Fedaykin{self.id} partially destroyed spice field at ({(self.i, self.j)})")
+            self.gamestate.update_spice_field_ref.remote(spice_field, self.i, self.j, "northern")
+            return True
+
+        else:
+            print(f"Fedaykin{self.id} tried to destroy spice at {(self.i, self.j)} but current vs. destruction is: ({spice_writes}) vs. ({list(write_order)})")
+            return False
+
+
+    def _move(self, new_i: int, new_j: int) -> None:
+        """
+        DO NOT MODIFY
+
+        Moves your Fedaykin to the coordinates (new_i, new_j) and sleeps for the
+        appropriate travel duration.
+        """
+        assert 0 <= new_i and new_i < MAP_DIM, f"New coord. i: {new_i} is off the map"
+        assert 0 <= new_j and new_j < MAP_DIM, f"New coord. i: {new_j} is off the map"
+
+        # calculate manhattan distance of movement
+        delta_i = abs(new_i - self.i)
+        delta_j = abs(new_j - self.j)
+        total_dist = delta_i + delta_j
+
+        # sleep for travel duration
+        time.sleep(total_dist * SLEEP_SECONDS_PER_TRAVEL_COORD)
+
+        # update coordinates
+        self.i = new_i
+        self.j = new_j
+
+
+
+@ray.remote(num_cpus=0.8, name="Fedaykin1", resources={"worker1": 1e-4})
+class Fedaykin1(BaseFedaykin):
+    """
+    Fedaykin warrior/actor running on Worker 1 on one of its two CPUs.
+    """
+    def __init__(self, payload: str):
+        super().__init__(payload)
+        self.id = 1
+
+
+    def start(self, spice_loc_map: np.ndarray, spice_file_map: np.ndarray, order_map: dict) -> None:
+        """
+        This is the entrypoint for your first actor's code. Your 4 actors will run in parallel
+        distributed across 4 CPUs (each actor runs on 1 CPU, there are 2 CPUs per machine,
+        thus 2 actors per node (sometimes also called "worker")).
+
+        The inputs to this function are as follows:
+
+        spice_loc_map: is a 2D numpy array of shape (MAP_DIM, MAP_DIM).
+        - if spice_loc_map[i,j] == 1, then there is a spice field at that location
+
+        spice_file_map: is a 2D numpy array of shape (MAP_DIM, MAP_DIM).
+        - if spice_file_map[i,j] == 2, then the data file for that spice field is on S3
+          (i.e. it will take longer to fetch when calling _destroy_spice_field())
+        - if spice_file_map[i,j] == 1, then the data file for that spice field is in Ray's object store
+          (i.e. it is in memory on this machine or another one in the cluster and will take less time
+           to fetch -- relative to S3 -- when calling _destroy_spice_field())
+
+        order_map: is a dictionary mapping (int, int) tuples to 1D numpy arrays of length 1, 2, 3, or 4
+        - for example order_map[(i,j)] might look like one of:
+            - array([2])
+            - array([3,1,4])
+            - array([4,3,1,2])
+        - the array specifies (left-to-right) the order in which Fedaykin actors must call _destroy_spice_field()
+          for it to be destroyed, for example:
+            - if order_map[(i,j)] == array([3,1,4]), then in order to destroy the spice field at location (i,j)
+              - first, Fedaykin3 must move to (i, j) and execute _destroy_spice_field()
+              - second, Fedaykin1 must move to (i, j) and execute _destroy_spice_field()
+              - finally, Fedaykin4 must move to (i, j) and execute _destroy_spice_field()
+        """
+        # I will set these state variables for you
+        self.spice_loc_map = spice_loc_map
+        self.spice_file_map = spice_file_map
+        self.order_map = order_map
+
+        # TODO: YOUR CODE GOES HERE
+        out = np.where(spice_loc_map==1)
+        for i, j in zip(out[0], out[1]):
+            if np.array_equal([self.id], self.order_map[(i,j)]):
+                break
+
+        self._move(i, j)
+        self._destroy_spice_field()
+
+
+@ray.remote(num_cpus=0.8, name="Fedaykin2", resources={"worker1": 1e-4})
+class Fedaykin2(BaseFedaykin):
+    """
+    Fedaykin warrior/actor running on Worker 1 on one of its two CPUs.
+    """
+    def __init__(self, payload: str):
+        super().__init__(payload)
+        self.id = 2
+
+
+    def start(self, spice_loc_map: np.ndarray, spice_file_map: np.ndarray, order_map: dict):
+        """
+        See comment for start() in Fedaykin1.
+        """
+        # I will set these state variables for you
+        self.spice_loc_map = spice_loc_map
+        self.spice_file_map = spice_file_map
+        self.order_map = order_map
+
+        # TODO: YOUR CODE GOES HERE
+        pass
+
+
+@ray.remote(num_cpus=0.8, name="Fedaykin3", resources={"worker2": 1e-4})
+class Fedaykin3(BaseFedaykin):
+    """
+    Fedaykin warrior running on Worker 2 on one of its two CPUs.
+    """
+    def __init__(self, payload: str):
+        super().__init__(payload)
+        self.id = 3
+
+
+    def start(self, spice_loc_map: np.ndarray, spice_file_map: np.ndarray, order_map: dict):
+        """
+        See comment for start() in Fedaykin1.
+        """
+        # I will set these state variables for you
+        self.spice_loc_map = spice_loc_map
+        self.spice_file_map = spice_file_map
+        self.order_map = order_map
+
+        # TODO: YOUR CODE GOES HERE
+        pass
+
+
+@ray.remote(num_cpus=0.8, name="Fedaykin4", resources={"worker2": 1e-4})
+class Fedaykin4(BaseFedaykin):
+    """
+    Fedaykin warrior running on Worker 2 on one of its two CPUs.
+    """
+    def __init__(self, payload: str):
+        super().__init__(payload)
+        self.id = 4
+
+
+    def start(self, spice_loc_map: np.ndarray, spice_file_map: np.ndarray, order_map: dict):
+        """
+        See comment for start() in Fedaykin1.
+        """
+        # I will set these state variables for you
+        self.spice_loc_map = spice_loc_map
+        self.spice_file_map = spice_file_map
+        self.order_map = order_map
+
+        # TODO: YOUR CODE GOES HERE
+        pass
+
+
+if __name__ == "__main__":
+    """
+    DO NOT MODIFY
+    """
+    # parse arguments
+    parser = argparse.ArgumentParser(description='Run an iteration of the Dune Game')
+    parser.add_argument('--verbose', default=True, action='store_true', help='Print verbose output')
+    parser.add_argument('--rival', type=str, help='The rival algorithm')
+    args = parser.parse_args()
+
+    # The user has to indicate the rival
+    if args.rival is None:
+        print("Please provide a rival")
+        exit(1)
+
+    elif args.rival not in ["noop", "silly-goose", "glossu-rabban", "feyd-rautha"]:
+        print(f"You specified rival: {args.rival}, but --rival must be one of ['noop', 'glossu-rabban', 'feyd-rautha', 'sardaukar']")
+
+    # connect to ray cluster
+    ray.init()
+
+    # print Ray job id in all caps
+    print(f"JOB ID IS: {ray.runtime_context.get_runtime_context().get_job_id()}")
+
+    # create game state
+    gs = GameState.remote()
+
+    # create fedaykin warriors
+    fedaykin_actors = [
+        Fedaykin1.remote("fedaykin"),
+        Fedaykin2.remote("fedaykin"),
+        Fedaykin3.remote("fedaykin"),
+        Fedaykin4.remote("fedaykin"),
+    ]
+
+    # create rival warriors
+    if args.rival == "noop":
+        rival_actors = [
+            Noop12.remote("rival"),
+            Noop12.remote("rival"),
+            Noop34.remote("rival"),
+            Noop34.remote("rival"),
+        ]
+
+    # set actors in gamestate
+    gs.set_actors.remote(fedaykin_actors, rival_actors)
+
+    # start game
+    start_time_ref = gs.start_game.remote()
+    start_time = ray.get(start_time_ref)
+
+    # let the game run for 100 seconds
+    while time.time() - start_time < 5.0:
+        time.sleep(1.0)
+
+    # end the game and return total spice destroyed by each player
+    total_spice_values_ref = gs.end_game.remote()
+    fedaykin_total_spice_destroyed, rival_total_spice_destroyed = ray.get(total_spice_values_ref)
+
+    # determine the winner and print the outcome
+    print(f"FEDAYKIN TOTAL SPICE DESTROYED: {fedaykin_total_spice_destroyed}")
+    print(f"RIVAL    TOTAL SPICE DESTROYED: {rival_total_spice_destroyed}")
+    print(f"-------------------------------------")
+    if fedaykin_total_spice_destroyed > rival_total_spice_destroyed:
+        print(f"THE WINNER IS: FEDAYKIN")
+    elif fedaykin_total_spice_destroyed < rival_total_spice_destroyed:
+        print(f"THE WINNER IS: RIVAL")
+    else:
+        print(f"THE WINNER IS: IT'S A DRAW")
