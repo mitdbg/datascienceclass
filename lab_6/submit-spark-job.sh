3654b0cfab9ac4e97cf7d172565354e544bccced
Xinjing Zhou <xinjing@mit.edu>
2024-04-21 22:09:48 -0400

    Update submit-spark-job.sh

--- lab_6/submit-spark-job.sh
+++ lab_6/submit-spark-job.sh
@@ -2,6 +2,6 @@
 random_filename=`cat /dev/urandom | tr -cd 'a-f0-9' | head -c 32`.py
 cat $1  > $random_filename
 
-scp -o StrictHostKeyChecking=no -i key.pem $random_filename hadoop@ec2-18-222-150-150.us-east-2.compute.amazonaws.com:/home/hadoop/
-ssh -o StrictHostKeyChecking=no -i key.pem hadoop@ec2-18-222-150-150.us-east-2.compute.amazonaws.com "spark-submit --master yarn /home/hadoop/$random_filename; rm /home/hadoop/$random_filename" > $1.stdout  2> $1.stderr
+scp -o StrictHostKeyChecking=no -i key.pem $random_filename hadoop@ec2-3-137-145-173.us-east-2.compute.amazonaws.com:/home/hadoop/
+ssh -o StrictHostKeyChecking=no -i key.pem hadoop@ec2-3-137-145-173.us-east-2.compute.amazonaws.com "spark-submit --master yarn /home/hadoop/$random_filename; rm /home/hadoop/$random_filename" > $1.stdout  2> $1.stderr
 rm $random_filename
